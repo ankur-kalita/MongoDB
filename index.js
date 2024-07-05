3ba6ac60ddd054aafa86b3cb6fc885dc966dfdd2
AnkuR0124 <ankur.23bcs10185@ms.sst.scaler.com>
2024-07-05 10:24:02 +0530

    commited

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://forgivemeankur11:ckByS05QmMlqAJUf@riboru69.cto6xzz.mongodb.net/?retryWrites=true&w=majority&appName=Riboru69"
+    ).then(() => {
+    console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+    console.log('Failed to connect to MongoDB', err)
+    })
+const app = express()
+
+app.listen(8086, ()=>{
+    console.log('Server is running on port 8086')
+})
